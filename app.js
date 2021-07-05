0ed5c613bfc10ea455d7c49044fa8b6aeca70da6
Pablo Daniel Sueiro <pablodsueiro@yahoo.com.ar>
2021-07-05 17:33:41 -0300

    third

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
-var mysql = require('mysql2');
+// var mysql = require('mysql2');
 
-var connection = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password:'1158025575',
-        database: 'utn'
-    }
-)
-connection.connect(function (error) 
-{
-    if(error){
-        console.log('sale error' + error.stack);
-        return;
-    }
-    console.log('success');
-});
+// var connection = mysql.createConnection(
+//     {
+//         host: 'localhost',
+//         user: 'root',
+//         password:'1158025575',
+//         database: 'utn'
+//     }
+// )
+// connection.connect(function (error) 
+// {
+//     if(error){
+//         console.log('sale error' + error.stack);
+//         return;
+//     }
+//     console.log('success');
+// });
 
-connection.query('select * from empleado', function (error, results)
-{
-    if(error){
-        console.log('sale otro error' + error.stack)
-    }
+// connection.query('select * from empleado', function (error, results)
+// {
+//     if(error){
+//         console.log('sale otro error' + error.stack)
+//     }
     
-    results.forEach(result => {
-        console.log(result);
-    });
-});
+//     results.forEach(result => {
+//         console.log(result);
+//     });
+// });
+
+const temp = ["a","b","c","d","e"]
+for (i=0;i<temp.length;i++)
+{
+    document.write(temp[i]);
+}
